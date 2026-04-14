ba102a3218e8fcd9e33ee9c9bf0f1c6da6b87e1c
Michael Franks <mffranks34@gmail.com>
2026-04-14 10:14:32 +0100

    added a function to handle input text

--- dice_roller/src/main.rs
+++ dice_roller/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
+
+fn string_input(prompt: String) -> String {
+    loop {
+        let mut input = String::new();
+
+        println!("{}", prompt);
+
+        if let Err(e) = io::stdin().read_line(&mut input) {
+            eprintln!("Failed to read: {}", e);
+            continue;
+        }
+
+        let trimmed = input.trim().to_string();
+
+        if trimmed.is_empty() {
+            eprintln!("Invalid input, please enter a valid string...");
+            continue;
+        }
+        return trimmed;
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    /*
+    define goal: create a system that takes in an input like 1d6 or 2d8
+    and give a number output, and the give the dices totals...
+    */
+    let name = string_input(String::from("Enter your name: "));
+    println!("Your name is: {}", name);
 }
